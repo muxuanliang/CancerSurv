e8e8090464b44e6bb269dd9adf2e8adab9ac9cc1
Muxuan Liang <10004752+muxuanliang@users.noreply.github.com>
2021-08-02 16:24:50 -0700

    add cancerSurv

---
+++ R/cancerSurv.R
@@ -0,0 +1,191 @@
+cancerSurv=function(subjectId=NULL, timeToEvent=NULL, measureTime=NULL, covariate=NULL, censoringIndicator=NULL, tau0=6, tradeoff=0.75, method=c("osf", "sm", "pc_glm"), methodFitCensoring="km"){
+  numberCovariate <- NCOL(covariate)
+  if (is.null(colnames(covariate))){
+    colnames(covariate) <- paste0("v", 1:(NCOL(covariate)))
+  }
+  dataFrame = data.frame(covariate, subjectId=subjectId, timeToEvent=timeToEvent, measureTime=measureTime, censoringIndicator=censoringIndicator)
+
+  times=as.numeric(names(table(dataFrame$measureTime)))
+  stopifnot(all(dataFrame$timeToEvent>=dataFrame$measureTime))
+
+  dataFrame$indicator=ifelse(dataFrame$timeToEvent-dataFrame$measureTime<=tau0, 1,0)
+
+  ipc <- NULL
+  for(i in 1:length(times)){
+    sub_data=dataFrame[dataFrame$measureTime==times[i],]
+    sub_data$ipcw=rep(0, nrow(sub_data))
+    sub_data$indicator=ifelse(sub_data$timeToEvent-sub_data$measureTime<=tau0, 1,0)
+    censored= 1-sub_data$censoringIndicator
+    if (methodFitCensoring=="km"){
+      km <- survival::survfit(survival::Surv(sub_data$timeToEvent,censored)~1)
+      survest <- stepfun(km$time, c(1, km$surv))
+      sub_data$ipcw[sub_data$indicator==1]=survest(sub_data$timeToEvent[sub_data$indicator==1])
+      sub_data$ipcw[sub_data$indicator==0]=survest(tau0+times[i])
+    } else if (methodFitCensoring=="cox"){
+      coxfit <- survival::coxph(survival::Surv(sub_data$timeToEvent,censored)~sub_data[,1:numberCovariate])
+      sub_data$ipcw[sub_data$indicator==1]=predict(coxfit, newdata=sub_data$timeToEvent[sub_data$indicator==1], type="survival")
+      sub_data$ipcw[sub_data$indicator==0]=predict(coxfit, newdata=u+times[i], type="survival")
+    }
+    idx=NA
+    for(k in 1:nrow(sub_data)){
+      idx[k]=which(sub_data$subjectId[k]==dataFrame$subjectId)[i]
+    }
+    ipc[idx]=sub_data$ipcw
+  }
+  dataFrame$ipc=ipc
+
+  denomm <- NULL
+  for(i in 1:length(times)){
+    sub_data=dataFrame[dataFrame$measureTime==times[i],]
+    idx=which(sub_data$censoringIndicator==1|(sub_data$censoringIndicator==0&sub_data$indicator==0))
+    sub_data=sub_data[idx,]
+    denomm[i]=sum(ifelse(sub_data$timeToEvent-sub_data$measureTime<=tau0, 1,0)/sub_data$ipc)/sum(1/sub_data$ipc)
+  }
+
+  if (method=="osf"){
+    weighted=weight=denom=NA
+    for(i in 1:nrow(dataFrame)){
+      sub_data=dataFrame[dataFrame$measureTime==dataFrame$measureTime[i],]
+      idd=dataFrame$measureTime[i]/6+1
+      denom[i]=denomm[idd]
+      w1=ifelse((dataFrame$timeToEvent[i]-dataFrame$measureTime[i])<=tau0, 1, 0)/denom[i]
+      w2=ifelse((dataFrame$timeToEvent[i]-dataFrame$measureTime[i])>tau0, 1, 0)/(1-denom[i])
+      if(dataFrame$indicator[i]==0|dataFrame$censoringIndicator[i]==1){
+        weight[i]=(w1-(tradeoff*w2))/dataFrame$ipc[i]
+      } else {
+        weight[i]=0
+      }
+      weighted[i]=weight[i]/sum(1/sub_data$ipc)
+    }
+
+    Idx=which(dataFrame$censoringIndicator==1|(dataFrame$censoringIndicator==0&dataFrame$indicator==0))
+    dataFrame=dataFrame[Idx,]
+    dataFrame$weight=weight[Idx]
+    A=sign(weighted)[Idx]
+    pr=rep(0, length(A))
+    pr[A==1]=sum(A==1)/length(A)
+    pr[A==-1]=sum(A==-1)/length(A)
+    dataFrame$pr=pr
+
+    covariate=dataFrame[1:numberCovariate]
+    outt=cbind(abs(weighted)[Idx]*pr, A, covariate)
+    colnames(outt)=c("outcome", "A", paste0("v", 1:(NCOL(covariate))))
+    outt=as.data.frame(outt)
+    outt[,2]=as.character(A)
+    moPropen <- modelObj::buildModelObj(model = ~1,solver.method = 'glm',
+                              solver.args = list('family'='binomial'),
+                              predict.method = 'predict.glm',
+                              predict.args = list(type='response'))
+
+    tmp_text <- "v1"
+    if (NCOL(covariate)>=2){
+      for (iter in 2:(NCOL(covariate))){
+        tmp_text <- paste0(tmp_text, "+v", iter)
+      }
+    }
+    expr <- paste0("earlRes=try(DynTxRegime::owl(moPropen = moPropen,
+                    data = outt, reward=outt$outcome, txName = 'A',
+                    regime = ~ ",tmp_text,",lambdas =c(1,5, 10), cvFolds = 2,
+                    kernel = 'linear', surrogate = 'hinge',kparam = NULL), TRUE)")
+    eval(expr = parse(text=expr))
+
+    coef_osf = DynTxRegime::regimeCoef(earlRes)
+    res = list(coef=stan(coef_osf))
+  } else if (method=="sm") {
+    covariate=covariate[which(dataFrame$censoringIndicator==1|(dataFrame$censoringIndicator==0&dataFrame$indicator==0)),]
+    dataFrame=dataFrame[which(dataFrame$censoringIndicator==1|(dataFrame$censoringIndicator==0&dataFrame$indicator==0)),]
+    glm_c=list()
+    for(i in 1:length(times)){
+      sub_data = dataFrame[dataFrame$measureTime==times[i],]
+      model=paste0("glm(indicator~", paste(colnames(covariate), collapse=" + "),",data=sub_data,weight=1/ipc,family='binomial')" )
+      glm_c[[i]]=(eval(parse(text=model)))
+    }
+
+    response=NA
+    for(i in 1:nrow(dataFrame)){
+      Id=which(dataFrame$measureTime[i]==times)
+      response[i]= predict(glm_c[[Id]], newdata=as.data.frame(t(as.data.frame(covariate[i,]))), type="response")
+    }
+    dataFrame$response=response
+
+    weight_glm = NULL
+    visit_index = dataFrame$measureTime/tau0+1
+    weight_glm = dataFrame$response/ denomm[visit_index]-tradeoff*((1-dataFrame$response)/(1- denomm[visit_index]))
+    dataFrame$weight_glm = weight_glm
+    model=paste0("geeglm(ifelse(weight_glm>0, 1, 0)~", paste(colnames(covariate), collapse=" + "),", id=subjectId, corstr='independence', data=dataFrame,family='binomial')" )
+    coef_sm=coef(eval(parse(text=model)))
+
+    res = list(coef=stan(coef_sm))
+  } else if (method=="pc_glm") {
+    id=(unique(dataFrame$subjectId))
+    idx=NA
+    for(i in 1:length(id)){
+      idx[i]=which(dataFrame$subjectId==id[i])[1]
+    }
+    sub_data=dataFrame[idx,]
+    sub_data$ipcw=rep(0, nrow(sub_data))
+    sub_data$indicator=ifelse(sub_data$timeToEvent-sub_data$measureTime<=tau0, 1,0)
+
+    censored= 1-sub_data$censoringIndicator
+    km <- survfit( Surv(sub_data$time,censored)~1)
+    survest <- stepfun(km$time, c(1, km$surv))
+
+    for(i in 1:length(times)){
+      sub_data=dataFrame[dataFrame$measureTime==times[i],]
+      sub_data$ipcw=rep(0, nrow(sub_data))
+      sub_data$ipcw[which(sub_data$indicator==1)]=survest(sub_data$timeToEvent[which(sub_data$indicator==1)])
+      sub_data$ipcw[which(sub_data$indicator==0)]=survest(tau0+times[i])
+      idx=NA
+      for(k in 1:nrow(sub_data)){
+        idx[k]=which(sub_data$subjectId[k]==dataFrame$subjectId & dataFrame$measureTime==times[i])
+      }
+      ipc[idx]=sub_data$ipcw
+    }
+    dataFrame$ipcc=ipc
+
+    covariate=covariate[which(dataFrame$censoringIndicator==1|(dataFrame$censoringIndicator==0&dataFrame$indicator==0)),]
+    dataFrame=dataFrame[which(dataFrame$censoringIndicator==1|(dataFrame$censoringIndicator==0&dataFrame$indicator==0)),]
+
+    model=paste0("glm(indicator~", paste(colnames(covariate), collapse=" + "),",data=dataFrame,weight=1/ipcc,family='binomial')" )
+    pc_glm=(eval(parse(text=model)))
+
+    prob=predict(pc_glm, type="response", newdata=data.frame(covariate))
+    dataFrame$risk_score=prob
+
+    Cutoff=seq(1:100)*0.01
+    Phi=Tpf=Fpf=NA
+    visit_index=dataFrame$measureTime/tau0+1
+    for(kkl in 1:length(Cutoff)){
+      cutoff=Cutoff[kkl]
+      cutoff=rep(cutoff, 5)
+      rule=NA
+      for(i in 1:nrow(dataFrame)){
+        rule[i]=ifelse(dataFrame$risk_score[i]>cutoff[visit_index[i]], 1, -1)
+      }
+
+      dataFrame$rule=rule
+      phi=tpf=fpf=NA
+      for(i in 1:length(times)){
+        sub_data=dataFrame[dataFrame$measureTime==times[i],]
+        idx=which(sub_data$censoringIndicator==1|(sub_data$censoringIndicator==0&sub_data$indicator==0))
+        sub_data=sub_data[idx,]
+        sub1=sub_data[(sub_data$timeToEvent-sub_data$measureTime)<=tau0,]
+        sub2=sub_data[(sub_data$timeToEvent-sub_data$measureTime)>tau0,]
+        phi[i]=sum(sub1$rule==1)/nrow(sub1)-tradeoff*sum(sub2$rule==1)/nrow(sub2)
+        phi[i]=sum( ifelse(sub1$rule==1, 1, 0)/sub1$ipc)/sum(1/sub1$ipc)-tradeoff*(sum( ifelse(sub2$rule==1, 1, 0)/sub2$ipc)/sum(1/sub2$ipc))
+        tpf[i]=sum( ifelse(sub1$rule==1, 1, 0)/sub1$ipc)/sum(1/sub1$ipc)
+        fpf[i]=sum( ifelse(sub2$rule==1, 1, 0)/sub2$ipc)/sum(1/sub2$ipc)
+      }
+
+      Phi[kkl]=mean(phi)
+      Tpf[kkl]=mean(tpf)
+      Fpf[kkl]=mean(fpf)
+    }
+    idd=which(Phi==max(Phi))
+    cutoff=Cutoff[idd]
+    PC_GLM=c(coef(pc_glm)[1]-log(cutoff/(1-cutoff)), coef(pc_glm)[-1])
+
+    res = list(coef=stan(PC_GLM))
+  }
+  return(res)
+}
